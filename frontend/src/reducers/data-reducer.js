eb06a3f96631b0f93769388cea0e4f2ec801f574
Logan Cooper <ldtcoop@gmail.com>
2022-03-02 15:07:41 -0500

    frontend Clean up data reducer

---
+++ frontend/src/reducers/data-reducer.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    current: [],
+    historical: []
+};
+
+export const dataSlice = createSlice({
+    name: 'data',
+    initialState,
+    // The `reducers` field lets us define reducers and generate associated actions
+    reducers: {
+        // placeholders for our data fetching actions
+        getCurrentData: (state) => state,
+        getHistoricalData: (state) => state,
+    },
+});
+
+export const { getCurrentData, getHistoricalData } = dataSlice.actions;
+
+export default dataSlice.reducer;
