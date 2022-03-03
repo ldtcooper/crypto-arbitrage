14014d629c1ba1fd04b841c34bd198fbf0c50aca
Logan Cooper <ldtcoop@gmail.com>
2022-03-02 19:30:37 -0500

    frontend Bring in skeleton table component

--- frontend/src/components/arbitrage.jsx
+++ frontend/src/components/arbitrage.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { DataTable } from './';
 
 const ArbitragePage = () => {
     return (
         <main>
             <h1>Arbitrage</h1>
+            <DataTable />        
         </main>
     );
 }
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./header.jsx";
 export { default as LineGraph } from "./linegraph.jsx";
 export { default as ArbitragePage } from "./arbitrage.jsx";
 export { default as HistoryPage } from "./history.jsx";
+export { default as DataTable } from "./datatable.jsx";
