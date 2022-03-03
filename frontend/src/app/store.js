346a266e3f48251765b2392138c4e23530f2733b
Logan Cooper <ldtcoop@gmail.com>
2022-03-03 14:00:04 -0500

    frontend Hook up data reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { dataSlice } from '../reducers';
+import logger from 'redux-logger'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    data: dataSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
